36cc155a31ece3b3a66c9b8c6e618bd9742d45aa
David Squire <david.squire@unimelb.edu.au>
2020-09-18 12:20:04 +1000

    Slice by slice destructive distance search working.
    Also performance improvements in gathering and parsing query results.

--- Scratch.py
+++ Scratch.py
@@ -2,6 +2,7 @@
 
 import sys
 import pickle
+import collections
 
 project_dir = "/Users/davids/git/WEHI_CoViD_RNAseq/RNAseq-extract-bar-codes"
 sys.path.insert(0, project_dir)
@@ -9,7 +10,7 @@ sys.path.insert(0, project_dir)
 from UMIHashTrie import UMIHashTrie
 from UMIData import UMIData
 
-umi_trie_filename = '/Users/davids/OneDrive - The University of Melbourne/WEHI Rory CoVid19 RNA/DB CloudStor 20200812/Standard_R1.fastq.gz_UMIHashTrie.pkl'
+umi_trie_filename = project_dir + '/Standard_R1.fastq.gz_UMIHashTrie.pkl'
 
 # load UMIHashTrie from file
 umi_trie_file = open(umi_trie_filename, 'rb')
@@ -19,6 +20,13 @@ umi_trie_file.close()
 # get UMIs in descending order of count
 sorted_umis = sorted(umi_trie.entries.items(), key = lambda x : x[1].count, reverse = True)
 
+# Example query
+matches = umi_trie.full_query_with_errors(sorted_umis[0][0], 1)
+sorted_matches = [v for k, v in sorted(matches.items(), key = lambda x : x[1]['trie_object'].count, reverse = True)]
+for match in sorted_matches:
+  print(f'Num match errors: ', match['num_errors'])
+  print(match['trie_object'])
+  
 # get UMI count distribution
 counts = [(x[0], x[1].count) for x in sorted_umis]
 # write this as a CSV
@@ -28,8 +36,35 @@ for key, count in counts:
   csv_file.write(f'%s,%d\n' % (key, count))
 csv_file.close()
   
-# write a CSV for a number of neighbours distribution
-max_neighbours = 4
+# # write a CSV for a number of neighbours distribution
+# max_neighbours = 4
+# min_count = 100 # from eyeballing a plot, at present
+# if counts[-1][1] < min_count:
+#   max_row = [n for n, x in enumerate( [x[1] for x in counts] ) if x < min_count][0]
+# else:
+#   max_row = len(sorted_umis)
+# # max_row = 2 # test
+# dist_headers = ['dist' + str(x) for x in range(1, max_neighbours + 1)]
+# num_neighbours_filename = umi_trie_filename + '_UMI-num-neighbours.csv'
+# num_neighbours_file = open(num_neighbours_filename, 'w')
+# # print header
+# num_neighbours_file.write('UMI,count,' + ','.join(dist_headers) + '\n')
+# 
+# # print data rows
+# for row in range(max_row):
+#   matches = umi_trie.full_query_with_errors(sorted_umis[row][0], max_neighbours)
+#   distances = [x['num_errors'] for x in matches.values()]
+#   dist_counts_hash = collections.Counter(distances)
+#   dist_counts = [str(dist_counts_hash[dist]) for dist in range(1, max_neighbours + 1)] # ignore first element, which is distance 0 from self
+#   row_string = ','.join([sorted_umis[row][0]] + [str(counts[row][1])] + dist_counts)
+#   num_neighbours_file.write(row_string + '\n')
+#   if row % 100 == 0:
+#     print(f'Row %d of %d: %s' % (row, max_row, row_string))
+#     num_neighbours_file.flush() # for progress monitoring with tail -f
+# num_neighbours_file.close()
+
+# write a CSV for a destructive number of neighbours distribution
+max_neighbours = 8
 min_count = 100 # from eyeballing a plot, at present
 if counts[-1][1] < min_count:
   max_row = [n for n, x in enumerate( [x[1] for x in counts] ) if x < min_count][0]
@@ -37,32 +72,29 @@ else:
   max_row = len(sorted_umis)
 # max_row = 2 # test
 dist_headers = ['dist' + str(x) for x in range(1, max_neighbours + 1)]
-num_neighbours_filename = umi_trie_filename + '_UMI-num-neighbours.csv'
+num_neighbours_filename = umi_trie_filename + '_UMI-num-neighbours-8_destructive.csv'
 num_neighbours_file = open(num_neighbours_filename, 'w')
 # print header
 num_neighbours_file.write('UMI,count,' + ','.join(dist_headers) + '\n')
 
+# compute distances one distance slice at a time, since is destructive (i.e. an n+1_neighbour of a centre could be the n_neighbour of another)
+distance_cols = []
+for dist in range(1, max_neighbours + 1):
+  print(f'Finding %d-neighbours, and destroying...' % dist)
+  matches_col = [umi_trie.full_query_with_errors(sorted_umis[row][0], dist, destructive = True) for row in range(max_row)]
+  distances_col = [[x['num_errors'] for x in matches.values() if x['num_errors'] != 0] for matches in matches_col]
+  dist_counts_col = [str(len(matches)) for matches in distances_col] # no need to count separately as destruction means there can't be any closer than dist
+  distance_cols.append(dist_counts_col)
+print('Transposing distance data...')
+distance_rows = [list(distance_col) for distance_col in zip(*distance_cols)] # transpose
 # print data rows
-for row in range(max_row):
-  num_neighbours = [len(umi_trie.full_query_with_errors(sorted_umis[row][0], col)) - 1 for col in range(1, 5)]
+for row in range(len(distance_rows)):
   row_string = ','.join([sorted_umis[row][0]] + \
     [str(counts[row][1])] + \
-    [str(len(umi_trie.full_query_with_errors(sorted_umis[row][0], col)) - 1) for col in range(1, 5)]
+    distance_rows[row]
   )
   num_neighbours_file.write(row_string + '\n')
   if row % 100 == 0:
     print(f'Row %d of %d: %s' % (row, max_row, row_string))
-    num_neighbours_file.flush() # for progress monitoring
+    num_neighbours_file.flush() # for progress monitoring with tail -f
 num_neighbours_file.close()
-quit()
-
-# print the largest one
-print(sorted_umis[0][0])
-# get the UMIs within a distance of one of this
-print(umi_trie.full_query_with_errors(sorted_umis[0][0], 1))
-
-
-print('Test with umi_trie2')
-result = umi_trie2.full_query_with_errors(UMIs[0], 8)
-for umi_id in result.keys():
-  print(result[umi_id]['trie_object'])
